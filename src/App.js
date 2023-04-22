8fd59dc782e532fa557a2ffdfb71ac8ccc5df37c
Mario Alberto Vasquez Arias Vasquez Arias <mario.vasquez2511@gmail.com>
2023-04-21 20:29:59 -0500

    retornar cambios de learn react

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          UNINORTE MARIO
+          Learn React
         </a>
       </header>
     </div>
